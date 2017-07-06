5f6703e8313f5d953c97b8f20d72b23b00ddf747
Rafael <rsanthos@gmail.com>
2017-07-06 11:32:05 -0300

    filtrando os dados

--- app/Http/Controllers/Api/BanksController.php
+++ app/Http/Controllers/Api/BanksController.php
@@ -15,29 +15,33 @@ class BanksController extends Controller
     public function index(Request $request)
     {
         $limit = $request->all()['limit'] ?? 20;
+        
         $order = $request->all()['order'] ?? null;
-
         if($order !== null){
             $order = explode(',', $order);
         }
-
         $order[0] = $order[0] ?? 'id';
         $order[1] = $order[1] ?? 'asc';
 
+        $where = $request->all()['where'] ?? [];
+
+        $like = $request->all()['like'] ?? null;
+        if($like !== null){
+            $like = explode(',', $like);
+            $like[1] = '%'.$like[1].'%';
+        }
+
         $result = \App\Bank::orderBy($order[0], $order[1])
+            ->where(function($query) use ($like){
+                if($like){
+                    return $query->where($like[0], 'like', $like[1]);
+                }
+                return $query;
+            })
+            ->where($where)
             ->paginate($limit);
 
         return response()->json($result);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -58,17 +62,6 @@ class BanksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
