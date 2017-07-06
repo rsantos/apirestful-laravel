e2b953ab12e8a67a5146e68d2cd2a6d7a8af5217
Rafael <rsanthos@gmail.com>
2017-07-06 12:57:55 -0300

    implementando laravel passport

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::resource('/banks', 'Api\BanksController');
+    Route::resource('/accounts', 'Api\AccountsController');
 });
-
-Route::resource('/banks', 'Api\BanksController');
-Route::resource('/accounts', 'Api\AccountsController');
